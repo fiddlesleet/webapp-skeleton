86370e44e15ec74f95d6dc585751dfcc656f6d7e
fiddlesleet <chris.smythe30@gmail.com>
2018-05-06 17:47:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.use(express.logger("default"));
 
 // since it would be cumbersome to create a route for every single static asset,
 // place all .css, client-side .js and image files in one directory, in this case named public
-app.use(express.static('public'))
-
+// since it would be cumbersome to create a route for every single static asset,
+// place all .css, client-side .js and image files in one directory, in this case named public
+app.use('/', express.static('public'))
 /*
 * Part II: Route URL requests to a specific function that handles them
 * Note: A route is required for each page the application has 
